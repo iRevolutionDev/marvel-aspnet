d22298179d50e31d710b2e86e55b7bf28d316024
Revolution <tavinho.daniel@gmail.com>
2023-09-20 14:37:51 -0300

    feat: add Blazor and remove Privacy View and Action

--- Marvel/Program.cs
+++ Marvel/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
 
@@ -23,5 +24,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapBlazorHub();
 
 app.Run();
